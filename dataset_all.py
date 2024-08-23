1f8a2bfe711d69ca06a6a6ee69fee7d2dd4b51e3
longfafffa <162342240+longfafffa@users.noreply.github.com>
2024-08-23 12:11:36 +0800

    Update dataset_all.py

--- dataset_all.py
+++ dataset_all.py
@@ -120,6 +120,73 @@ class Train_dataset(data.Dataset):
     def __len__(self):
         return len(self.A_paths)
 
+class Train_undataset(data.Dataset):
+    def __init__(self, dataroot, phase, finesize, dataset):
+        super().__init__()
+        self.phase = phase
+        self.root = dataroot
+        self.fineSize = finesize
+        self.dir = os.path.join(self.root, self.phase, 'input', dataset)
+        # gt path
+        self.dir_gt = os.path.join(self.root, self.phase, 'candidate', dataset)
+
+        # get the dir
+        # input dir
+        dir = sorted(make_dataset(self.dir))
+        dir_gt = sorted(make_dataset(self.dir_gt))
+        # select snow_dir
+        if dataset == 'snow':
+            selected_id = random.sample(range(0, len(dir)), 4500)
+            self.A_paths = [dir[i] for i in selected_id]
+            self.B_paths = [dir_gt[i] for i in selected_id]
+        else:
+            self.A_paths = dir
+            self.B_paths = dir_gt
+        self.transform = ToTensor()  # [0,1]
+
+    def __getitem__(self, index):
+        A = Image.open(self.A_paths[index]).convert("RGB")
+        candidate = Image.open(self.B_paths[index]).convert('RGB')
+        resized_a = A
+        # resized_b = B
+        width, height = A.size
+        if width < self.fineSize and height < self.fineSize:
+            resized_a = A.resize((self.fineSize, self.fineSize), Image.ANTIALIAS)
+            # resized_b = B.resize((self.fineSize, self.fineSize), Image.ANTIALIAS)
+            width = self.fineSize
+            height = self.fineSize
+        elif width < self.fineSize:
+            resized_a = A.resize((self.fineSize, height), Image.ANTIALIAS)
+            # resized_b = B.resize((self.fineSize, height), Image.ANTIALIAS)
+            width = self.fineSize
+        elif height < self.fineSize:
+            resized_a = A.resize((width, self.fineSize), Image.ANTIALIAS)
+            # resized_b = B.resize((width, self.fineSize), Image.ANTIALIAS)
+            height = self.fineSize
+        # totate
+        x, y = (width - self.fineSize + 1) / 2, (height - self.fineSize + 1) / 2
+        degrad_patch_crop = resized_a.crop((x, y, x + self.fineSize, y + self.fineSize))
+        # strong augmentation
+        strong_data = data_aug(degrad_patch_crop)
+        tensor_w = self.transform(degrad_patch_crop)
+        tensor_s = self.transform(strong_data)
+        # tensor_c = self.transform(C)
+        tensor_d = self.transform(candidate)
+        name = self.B_paths[index]
+        return tensor_w, tensor_s, tensor_d, name
+
+    def update_paths(self):
+        self.dir = os.path.join(self.root, self.phase, 'input', 'snow')
+        self.dir_gt = os.path.join(self.root, self.phase, 'candidate', 'snow')
+        dir = sorted(make_dataset(self.dir))
+        dir_gt = sorted(make_dataset(self.dir_gt))
+        selected_id = random.sample(range(0, len(dir)), 4500)
+        self.A_paths = [dir[i] for i in selected_id]
+        self.B_paths = [dir_gt[i] for i in selected_id]
+
+    def __len__(self):
+        return len(self.A_paths)
+
 class Test_dataset(data.Dataset):
     def __init__(self, dataroot):
         super().__init__()
@@ -136,8 +203,51 @@ class Test_dataset(data.Dataset):
 
     def __len__(self):
         return len(self.A_paths)
-    
-    
+
+
+class ValLabeled(data.Dataset):
+    def __init__(self, dataroot, finesize):
+        super().__init__()
+        # self.phase = phase
+        self.root = dataroot
+        self.fineSize = finesize
+
+        self.dir_A = os.path.join(self.root, 'input')
+        self.dir_B = os.path.join(self.root, 'GT')
+
+        # image path
+        self.A_paths = sorted(make_dataset(self.dir_A))
+        self.B_paths = sorted(make_dataset(self.dir_B))
+
+        # transform
+        self.transform = ToTensor()  # [0,1]
+
+    def __getitem__(self, index):
+
+        A = Image.open(self.A_paths[index]).convert("RGB")
+        B = Image.open(self.B_paths[index]).convert("RGB")
+        resized_a = A
+        resized_b = B
+        width, height = A.size
+        if width < self.fineSize and height < self.fineSize:
+            resized_a = A.resize((self.fineSize, self.fineSize), Image.ANTIALIAS)
+            resized_b = B.resize((self.fineSize, self.fineSize), Image.ANTIALIAS)
+        elif width < self.fineSize:
+            resized_a = A.resize((self.fineSize, height), Image.ANTIALIAS)
+            resized_b = B.resize((self.fineSize, height), Image.ANTIALIAS)
+        elif height < self.fineSize:
+            resized_a = A.resize((width, self.fineSize), Image.ANTIALIAS)
+            resized_b = B.resize((width, self.fineSize), Image.ANTIALIAS)
+        x, y = (width - self.fineSize + 1) / 2, (height - self.fineSize + 1) / 2
+        degrad_patch_crop = resized_a.crop((x, y, x + self.fineSize, y + self.fineSize))
+        clean_patch_crop = resized_b.crop((x, y, x + self.fineSize, y + self.fineSize))
+        tensor_a = self.transform(degrad_patch_crop)
+        tensor_b = self.transform(clean_patch_crop)
+        return tensor_a, tensor_b
+
+    def __len__(self):
+        return len(self.A_paths)
+
 class Val_dataset(data.Dataset):
     def __init__(self, dataroot, finesize):
         super().__init__()
